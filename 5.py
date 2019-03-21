56fba26e9ffbbb134efe3d688f5039cf3712d185
Jonathan Yeung <jonathanyeung1997@gmail.com>
2019-03-21 12:14:23 +1100

    Configured question 5 and renamed q1 + q2

--- 5.py
+++ 5.py
@@ -1,27 +1,11 @@
-def primeDictionary(num):
-    primeSet={}
-    for i in range(2,num+1):
-        boolean = False
-        boolean = findPrime(i)
-        if boolean == True:
-            primeSet[i] = 0
-    return primeSet
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
 
-def findPrime(num):
-    for i in range(2,num):
-        if num % i == 0:
-            return False
-    return True
-
-def updateDictionary(primeSet, num):
-    for i in range(2,num+1):
-        for j in range(2,i+1):
-            if i % j == 0:
-                primeSet[j] = primeSet[j]+1
-
-    return primeSet
-
-
-primeSet = primeDictionary(20)
-primeSet = updateDictionary(primeSet, 20)
-print(primeSet)
+def lcm(a, b):
+    return (a * b) // gcd(a, b)
+    
+    
+import functools 
+print(functools.reduce(lambda x, y: lcm(x, y), range(1,20+1)))
